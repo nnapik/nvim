f84a3e8d574fec1a5a47492878257dcd19cafe86
Petr Nedoma <nnapik@gmail.com>
2023-10-05 20:32:36 +0200

    added init for neovide

--- lua/hellmen/init.lua
+++ lua/hellmen/init.lua
@@ -11,6 +11,18 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+
 require("hellmen.lazy")
 require("hellmen.remap")
 require("hellmen.set")
+
+--if opening neovide, change directory to current file's directory
+if vim.g.neovide then
+    -- if opening neovide with selecting a file, change directory to that file
+    if (#vim.v.argv) == 4 then
+        vim.cmd.cd("%:h")
+    -- if opening neovide on its own, change directory to the HOME
+    else
+        vim.cmd.cd(os.getenv("HOME"))
+    end
+end
