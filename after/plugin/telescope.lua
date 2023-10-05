18e357d749e4d76acd4d52cdc0cc28e0c888a0ca
Petr Nedoma <nnapik@gmail.com>
2023-10-05 20:34:30 +0200

    added more shortcuts for telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,12 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>gf', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ")});
-end)
-vim.keymap.set('n', '<leader>fb', builtin.buffers)
+vim.keymap.set('n', '<leader>ff', builtin.find_files)
+vim.keymap.set('n', '<leader>gf', builtin.live_grep)
+vim.keymap.set('v', '<leader>gf', builtin.grep_string)
+vim.keymap.set('n', '<leader>bb', builtin.buffers)
+vim.keymap.set('n', '<leader>kb', builtin.keymaps)
+vim.keymap.set('n', '<leader><leader>', builtin.commands)
+vim.keymap.set('n', '<leader>h', builtin.help_tags)
 
+require('telescope').setup{
+    file_ignore_patterns = { ".git"}
+}
