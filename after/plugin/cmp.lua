6aff384f30058799dbbb16a94866c20757a065f3
Petr Nedoma <pnedoma@moravia.com>
2023-10-05 15:46:30 +0200

    added config for cpm, (enter to confirm selection)

---
+++ after/plugin/cmp.lua
@@ -0,0 +1,17 @@
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+    preselect = 'item',
+    completion = {
+        completeopt = 'menu,menuone,noinsert'
+    },
+    mapping = cmp.mapping.preset.insert({
+        -- `Enter` key to confirm completion
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+
+        -- Navigate between snippet placeholder
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+    })
+})
