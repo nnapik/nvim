ba60c03ac7a4b48e0f9421c3b9e8539d2e809421
Petr Nedoma <pnedoma@moravia.com>
2023-10-05 15:45:48 +0200

    ignore "line too long for"

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,12 +1,23 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp.default_keymaps({ buffer = bufnr })
 end)
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require('lspconfig').pylsp.setup({
+    settings = {
+        pylsp = {
+            plugins = {
+                pycodestyle = {
+                    ignore = { 'E501' }
+                }
+            }
+        }
+    }
+})
 
 lsp.setup()
