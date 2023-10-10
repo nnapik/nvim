5c52de684b39e52ffe3a65fcfc4e9b285c0cf6ac
Petr Nedoma <pnedoma@moravia.com>
2023-10-10 10:57:49 +0200

    removed dlv bootstrap

--- lua/hellmen/bootstrap.lua
+++ lua/hellmen/bootstrap.lua
@@ -12,21 +12,3 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- go bootstrap
--- delve debugger
-local dlv_path = vim.fn.stdpath("data") .. "/delve"
-if not vim.loop.fs_stat(dlv_path) then
-    vim.fn.system({
-        'git',
-        'clone',
-        'https://github.com/go-delve/delve',
-        dlv_path
-    })
-    vim.fn.system({
-        'go',
-        'install',
-        'github.com/go-delve/delve/cmd/dlv',
-        dlv_path
-    })
-end
-
