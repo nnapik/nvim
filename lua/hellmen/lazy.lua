dc12c903d0f375d7bbd634002914c452d45a32ab
Petr Nedoma <pnedoma@moravia.com>
2024-01-02 10:51:59 +0100

    removed luasnip

--- lua/hellmen/lazy.lua
+++ lua/hellmen/lazy.lua
@@ -29,7 +29,6 @@ return require('lazy').setup({
     'hrsh7th/nvim-cmp',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-nvim-lua',
-    'L3MON4D3/LuaSnip',
     { 'VonHeikemen/lsp-zero.nvim', branch = 'v2.x' },
 
     -- golang
