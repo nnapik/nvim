043ac886391b73f85f6fd8d9d820aec3ae022ae3
Petr Nedoma <petr.nedoma@rws.com>
2024-08-01 09:27:03 +0200

    added VS-like dap keybinds

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -5,3 +5,9 @@ require("dap-python").setup('python3')
 --    name = 'Launch file';
 --    program = '${file}';
 --}
+vim.keymap.set("n", "<leader>tb", require("dap").toggle_breakpoint)
+vim.keymap.set("n", "<F9>", require("dap").toggle_breakpoint)
+vim.keymap.set("n", "<F5>", require("dap").continue)
+vim.keymap.set("n", "<F6>", require("dap").close)
+vim.keymap.set("n", "<F10>", require("dap").step_over)
+vim.keymap.set("n", "<F11>", require("dap").step_into)
