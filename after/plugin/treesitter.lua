8cfa5a725f204fc69639f0b228152ee7eb3c05f2
Petr Nedoma <petr.nedoma@rws.com>
2025-02-20 10:29:29 +0100

    separated each entry per line, to make git diff work

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,16 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "bash", "c_sharp", "dockerfile"},
+  ensure_installed = {
+      "python",
+      "c",
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "rust",
+      "bash",
+      "dockerfile"
+  },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
