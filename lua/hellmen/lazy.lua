dd58dfdb48299dad7b0bd658f56063fe4ccd0154
Petr Nedoma <petr.nedoma@rws.com>
2025-02-20 10:31:21 +0100

    modified lspconfig to include it's deps and config

--- lua/hellmen/lazy.lua
+++ lua/hellmen/lazy.lua
@@ -26,9 +26,25 @@ return require('lazy').setup({
 
     -- Language Server integration
     -- LSP Support
-    'neovim/nvim-lspconfig',
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
+    { "neovim/nvim-lspconfig",
+      dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim"
+      },
+      config = function()
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+        require('mason').setup()
+        local mason_lspconfig = require 'mason-lspconfig'
+        mason_lspconfig.setup {
+            ensure_installed = { "pyright" }
+        }
+        require("lspconfig").pyright.setup {
+            capabilities = capabilities,
+        }
+      end
+    },
     -- Autocompletion
     'hrsh7th/nvim-cmp',
     'hrsh7th/cmp-nvim-lsp',
